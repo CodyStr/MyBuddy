fefd865539951efd50f5b8c37ee2da29d0b9a796
Cody Strijbosch <codystrij678@gmail.com>
2017-05-29 10:42:38 +0200

    Added documentation in .PDF

---
+++ bootstrap/dbconfig/config.php
@@ -0,0 +1,21 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'mybuddy');
+$conn = DB();
+function DB()
+{
+	try {
+		$db = new PDO('mysql:host='.HOST.';dbname='.DATABASE.'',USER, PASSWORD);
+		return $db;
+	} catch (PDOException $e){
+		return "Error!: " . $e->getMessage();
+		die();
+	}
+}
+
+var_dump(DB());
+
+?>
